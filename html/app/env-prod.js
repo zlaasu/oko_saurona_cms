139407eef9c87a2cf18e90dbecbdb94995a5e46b
Janek <zlaasu@gmail.com>
2019-11-15 10:06:00 +0100

    [#] - add prod env

---
+++ html/app/env-prod.js
@@ -0,0 +1,4 @@
+var API_URL = 'https://okosaurona-api.zlasu.org/';
+var HASH = '#';
+var GMAPS = "AIzaSyBdIss9DT7hkFJzdxI_k6h8PelgwXGNs9Q";
+var FIREBASE_TOKEN;
---
+++ html/app/env.js
@@ -0,0 +1,6 @@
+// var API_URL = 'https://okosaurona.zlasu.org/backend/';
+var API_URL = 'http://localhost:8080/';
+// var MAIN_URL = 'https://okosaurona.zlasu.org/backend/#/';
+var HASH = '#';
+var GMAPS = "AIzaSyBdIss9DT7hkFJzdxI_k6h8PelgwXGNs9Q";
+var FIREBASE_TOKEN;
